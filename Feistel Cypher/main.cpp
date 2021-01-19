2c7984d5997e711b109e1a2e90878a8d5248e780
Rafael Teixeira <rodriguesteixeirarafael@gmail.com>
2021-01-19 13:24:16 -0300

    Organizing the repo

--- Feistel Cypher/main.cpp
+++ Feistel Cypher/main.cpp
@@ -1,9 +1,11 @@
 #include "feistel.h"
 
 int main(int argc, char** argv){
-    cout << "Feistel says:" << endl;
-    string key = "UMVAC@RA";
-    string encrypted = Feistel("RAFAELRA", key);
+    string input = "1!2.3;4#";
+    string key = "150d1400474c524100";
+    
+    cout << "Feistel says: " << input << endl;
+    string encrypted = Feistel(input, key);
     string decrypted = Feistel(encrypted, key);
 
     cout << "Encrypted to: " << toHex(encrypted) << endl;
